b1cb9376dee82733fe40c7ca220d99f944238138
HanyMaged4 <hany.maged9102@gmail.com>
2025-11-23 20:40:12 +0200

    same

--- backend/src/link/link.resolver.ts
+++ backend/src/link/link.resolver.ts
@@ -1,4 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
+import { Link } from 'src/GraphQLSchemas/link.model';
+import { LinkService } from './link.service';
+import { CreateLinkInput } from './DTOs/Create-links.dto';
 
 @Resolver()
-export class LinkResolver {}
+@Resolver(() => Link)
+export class LinkResolver {
+  constructor(private readonly linkService: LinkService) {}
+
+  @Mutation(() => Link)
+  addLink(@Args('createLinkInput') createLinkInput: CreateLinkInput) {
+    // return this.linkService.create(createLinkInput);
+  }
+
+  @Mutation(() => Link)
+  deleteLink(@Args('id', { type: () => Int }) id: number) {
+    // return this.linkService.delete(id);
+  }
+
+//   @Mutation(() => Link)
+//   updateLink(@Args('id', { type: () => Int }) id: number, @Args('updateLinkInput') updateLinkInput: UpdateLinkInput) {
+//     // return this.linkService.update(id, updateLinkInput);
+//   }
+}
